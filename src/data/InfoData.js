1d34beae22d7a3f2b20c54bbfeb29de39b8bbb36
Islam Ahmad <islamahmed1145@gmail.com>
2021-08-25 15:00:15 +0200

    commited from work 25-8-2021

---
+++ src/data/InfoData.js
@@ -0,0 +1,28 @@
+import ImageFive from '../images/ImageFive.jpg';
+import ImageSix from '../images/ImageSix.jpg';
+
+export const InfoData = {
+    heading: 'Explore our site beautiful homes',
+
+    paragraphOne: 'Occaecat cillum ex velit id est ad fugiat ex anim ut esse elit aute magna. Dolore Lorem labore pariatur voluptate ut elit. Labore proident Lorem est est adipisicing. Eu non dolore commodo ex non do id nulla deserunt. Excepteur reprehenderit cupidatat qui ',
+
+    paragraphTwo: 'Occaecat cillum ex velit id est ad fugiat ex anim ut esse elit aute magna. Dolore Lorem labore pariatur voluptate ut elit. Labore proident Lorem est est adipisicing. Eu non dolore commodo ex non do id nulla deserunt. Excepteur reprehenderit cupidatat qui ',
+
+    buttonLabel: 'View Homes',
+    image: ImageFive,
+    reverse: false,
+    delay: 100
+};
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+
+    paragraphOne: 'Occaecat cillum ex velit id est ad fugiat ex anim ut esse elit aute magna. Dolore Lorem labore pariatur voluptate ut elit. Labore proident Lorem est est adipisicing',
+
+    paragraphTwo: 'Occaecat cillum ex velit id est ad fugiat ex anim ut esse elit aute magna. Dolore Lorem labore pariatur voluptate ut elit. Labore proident Lorem est est adipisicing',
+
+    buttonLabel: 'View Homes',
+    imag: ImageSix,
+    reverse: true,
+    delay: 300
+};
